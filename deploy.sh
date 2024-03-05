1212249c191ad31a03f9612d4e4158453a846885
Nimrah12 <nimrahmemon12@gmail.com>
2024-03-05 14:55:38 +0000

    modify config file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
-sudo apt update && sudo apt install nodejs npm
+# Update system packages and install Node.js and npm
+sudo apt update && sudo apt install -y nodejs npm
+
+# Install pm2 globally
 sudo npm install -g pm2
+
+# Stop the pm2 process named "example_app"
 pm2 stop example_app
+
+# Navigate to the ExampleApplication directory
 cd ExampleApplication/
+
+# Install npm dependencies
 npm install
+
+# Start the pm2 process for your application
 pm2 start ./bin/www --name example_app
